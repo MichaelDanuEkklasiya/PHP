6ae5e6e5c5ed05c6bcc81cedf9895f14f06b8529
MichaelDanuEkklasiya <63625736+MichaelDanuEkklasiya@users.noreply.github.com>
2020-08-27 12:01:27 +0700

    Add files via upload

---
+++ Try.php
@@ -0,0 +1,16 @@
+<?php
+class BelajarOOP
+{
+ 
+  public function the_name()
+  {
+      echo 'Saya Belajar Di onphpid.com';
+  }
+  
+  public function get_name ()
+  {
+      return 'Saya Sedang Belajar OOP';
+  }
+ 
+}
+?>
